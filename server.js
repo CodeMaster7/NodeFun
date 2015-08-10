0631a73e719759aa8142d3a2b52f29cbdd920c52
CodeMaster7 <italianstalian_2@yahoo.com>
2015-08-10 10:54:58 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+/////////////////////////////////////////////////////////
+/////////////       Dependencies
+////////////////////////////////////////////////////////
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+var port =3000;
+
+app.listen(port, function () {
+    console.log('Im watching you server 3000 @.@');
+});
+
+
