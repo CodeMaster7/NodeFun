18b9f3eb2e63b481c1a570b42f1e1435f2d4fb4a
CodeMaster7 <italianstalian_2@yahoo.com>
2015-08-10 13:24:29 -0600

    commit number 3

--- server.js
+++ server.js
@@ -4,6 +4,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
+var request = require('request');
+var twilio = require('twilio');
 
 var app = express();
 var port =3000;
@@ -20,13 +22,20 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 app.use(cors());
 
+var message = {
+    "message": "Hello world"
+}
+
 /////////////////////////////////////////////////////////
 /////////////       Endpoints
 ////////////////////////////////////////////////////////
 app.get('/api/message', function (req, res) {
     return res.send({"message": "hello user!!!!"});
 });
-
+app.post('/api/receive_message', function (req, res) {
+    console.log(req.body.message);
+    res.send();
+});
 
 app.listen(port, function () {
     console.log('Im watching you server 3000 @.@');
