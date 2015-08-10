f976ab19d631382452e27f8b0941f7d3f216bfba
CodeMaster7 <italianstalian_2@yahoo.com>
2015-08-10 11:17:07 -0600

    second commit

--- server.js
+++ server.js
@@ -3,9 +3,30 @@
 ////////////////////////////////////////////////////////
 var express = require('express');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
 var app = express();
 var port =3000;
+
+/////////////////////////////////////////////////////////
+/////////////       Middleware
+////////////////////////////////////////////////////////
+
+app.use(express.static('public'));
+///// app.use(bodyparser());      ------normal
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+app.use(cors());
+
+/////////////////////////////////////////////////////////
+/////////////       Endpoints
+////////////////////////////////////////////////////////
+app.get('/api/message', function (req, res) {
+    return res.send({"message": "hello user!!!!"});
+});
+
 
 app.listen(port, function () {
     console.log('Im watching you server 3000 @.@');
